c30c2916618e292fc6b4f4848c712fcd888d3756
phonghpb <phonghc@hp-backup.com>
2023-03-03 10:28:19 +0900

    ?

---
+++ Attributes/NotifyMethodAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+[AttributeUsage(AttributeTargets.Property)]
+class NotifyMethodAttribute : Attribute
+{
+    public string[] Methods { get; set; }
+
+    public NotifyMethodAttribute(string[] iMethods)
+    {
+        Methods = iMethods;
+    }
+}
